299f89fdeab15dcca74612443d5fdba9449a769f
Udit Manav <uditmanav17@gmail.com>
2020-06-08 06:02:06 +0530

    pyc experiment

---
+++ pyc_test/base.py
@@ -0,0 +1,3 @@
+# these are my secret credentials
+KEY1='0100a75786009ef8'
+KEY2='fd219a1f549cc3db'
---
+++ pyc_test/sample.py
@@ -0,0 +1,9 @@
+from base import KEY1, KEY2
+
+KEY3='848c3a5a81b87c14'
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+if __name__ == "__main__":
+    print(KEY1, KEY2)
